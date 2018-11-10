f2f62c986965164561a162e086bacca158eb73d0
somnathbm <somnathbm0@gmail.com>
2018-11-10 15:33:15 +0530

    initial commit

--- app.py
+++ app.py
@@ -1,18 +1,43 @@
-from flask import Flask
-from datetime import datetime
+#Removed cv2 and Numpy libraries and using Scikit-Image library instead
+from __future__ import print_function
+from flask import Flask, make_response, jsonify, request
+from flask_restful import Resource, Api
+from skimage import io
+import pyzbar.pyzbar as pyzbar
+
+
 app = Flask(__name__)
+api = Api(app)
 
-@app.route('/')
-def homepage():
-    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
+def decode(im) : 
+  # Find QR codes
+  decodedObjects = pyzbar.decode(im)
+     
+  return decodedObjects
 
-    return """
-    <h1>Hello heroku</h1>
-    <p>It is currently {time}.</p>
+class myAPI(Resource):
+    @api.representation('application/json')
+    def post(self):
+        # First, get the url param from request body
+        json_data = request.get_json(force=True)
+        url_val = json_data['url']
 
-    <img src="http://loremflickr.com/600/400" />
-    """.format(time=the_time)
+        # Second, read the resource in url
+        try:
+            im = io.imread(url_val)
+            # Third, try to decode
+            decodedObjects = decode(im)
+            # If decode fails, it returns an empty array
+            if(len(decodedObjects) > 0):
+                return make_response(jsonify(data=decodedObjects[0][0], url=url_val, tag_res=True), 200)
+            return make_response(jsonify(data=[], url=url_val, tag_res= False), 200)
+        except:
+            return make_response(jsonify(error='bad request'), 400)
+    
+    def get(self):
+        return 'hello world!'
+
+api.add_resource(myAPI, '/tag_req')
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=True)
-
+    app.run(debug=True)
