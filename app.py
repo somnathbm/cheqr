df52a48b723235af5436a608c419380fc5f339d1
somnathbm <somnathbm0@gmail.com>
2018-11-10 20:36:29 +0530

    changelog - reverted to prev code

--- app.py
+++ app.py
@@ -3,19 +3,15 @@ from __future__ import print_function
 from flask import Flask, make_response, jsonify, request
 from flask_restful import Resource, Api
 from skimage import io
-#import pyzbar.pyzbar as pyzbar
-from qrtools import QR
+import pyzbar.pyzbar as pyzbar
 
 app = Flask(__name__)
 api = Api(app)
 
-def decode(url) : 
+def decode(im) : 
   # Find QR codes
-  # decodedObjects = pyzbar.decode(im)
-  qrCode = QR(filename = url)
-  status = qrCode.decode()
-  print('decode result', qrCode)
-  return qrCode.data
+  decodedObjects = pyzbar.decode(im)
+  return decodedObjects
 
 class myAPI(Resource):
     @api.representation('application/json')
@@ -26,19 +22,15 @@ class myAPI(Resource):
 
         # Second, read the resource in url
         try:
-            # im = io.imread(url_val)
+            im = io.imread(url_val)
             # Third, try to decode
-            decodedObjects = decode(url_val)
+            decodedObjects = decode(im)
             # If decode fails, it returns an empty array
-            # if(len(decodedObjects) > 0):
-            #     return make_response(jsonify(data=decodedObjects[0][0], url=url_val, tag_res=True), 200)
-            # return make_response(jsonify(data=[], url=url_val, tag_res= False), 200)
-            return make_response(jsonify(data=decodedObjects, url=url_val, tag_res= False), 200)
+            if(len(decodedObjects) > 0):
+                return make_response(jsonify(data=decodedObjects[0][0], url=url_val, tag_res=True), 200)
+            return make_response(jsonify(data=[], url=url_val, tag_res= False), 200)
         except:
             return make_response(jsonify(error='bad request'), 400)
-    
-    def get(self):
-        return 'hello world!'
 
 api.add_resource(myAPI, '/tag_req')
 
